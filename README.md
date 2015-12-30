5bb711a0817dcc988ec1ffb65b8fbb8432df135a
Michael Lore <michael.lore@gmail.com>
2015-12-30 12:13:41 -0500

    Initial revision

---
+++ README.md
@@ -0,0 +1,144 @@
+UNHTML
+======
+
+[![Build Status](https://travis-ci.org/ancientlore/unhtml.svg?branch=master)](https://travis-ci.org/ancientlore/unhtml)
+[![GoDoc](https://godoc.org/github.com/ancientlore/unhtml?status.svg)](https://godoc.org/github.com/ancientlore/unhtml)
+
+Package unhtml designed to remove HTML tags from text and do minor formatting updates.
+It is intended to avoid changing the text very much - in particular so that it doesn't
+mess up formatted plain text when HTML tags are not present. This allows you to run the
+converter on data that you received without needing to bother checking if HTML tags are
+in there.
+
+That said, the package is primarily intended to handle minor HTML snippets. It isn't a
+full-fledged formatter. Below are tags that are ignored, handled, and intentionally
+skipped.
+
+Ignored tags:
+
+	(comments)
+	DOCTYPE
+	abbr
+	acronym
+	address
+	area
+	article
+	aside
+	audio
+	b
+	base
+	basefont
+	bdi
+	bdo
+	big
+	body
+	button
+	canvas
+	caption
+	center
+	cite
+	code
+	col
+	colgroup
+	datalist
+	dd
+	del
+	details
+	dfn
+	dialog
+	dir
+	dl
+	dt
+	em
+	fieldset
+	figcaption
+	figure
+	font
+	footer
+	form
+	h1 to h6
+	header
+	html
+	i
+	input
+	ins
+	kbd
+	keygen
+	label
+	legend
+	main
+	map
+	mark
+	menu
+	menuitem
+	meter
+	nav
+	noframes
+	noscript
+	optgroup
+	option
+	output
+	param
+	progress
+	q
+	rp
+	rt
+	ruby
+	s
+	samp
+	section
+	select
+	small
+	source
+	span
+	strike
+	strong
+	sub
+	summary
+	sub
+	summary
+	sup
+	tbody
+	textarea
+	tfoot
+	thead
+	time
+	track
+	tt
+	u
+	var
+	video
+	wbr
+
+Skipped tags:
+
+	applet
+	embed
+	frame
+	frameset
+	head
+	iframe
+	link
+	meta
+	object
+	script
+	style
+	title
+
+
+Handled tags:
+
+	a
+	blockquote
+	br
+	div
+	hr
+	img
+	li
+	ol
+	p
+	pre
+	table
+	td
+	th
+	tr
---
+++ html2text.go
@@ -0,0 +1,329 @@
+/*
+	Package unhtml designed to remove HTML tags from text and do minor formatting updates.
+	It is intended to avoid changing the text very much - in particular so that it doesn't
+	mess up formatted plain text when HTML tags are not present. This allows you to run the
+	converter on data that you received without needing to bother checking if HTML tags are
+	in there.
+
+	That said, the package is primarily intended to handle minor HTML snippets. It isn't a
+	full-fledged formatter. Below are tags that are ignored, handled, and intentionally
+	skipped.
+
+	Ignored tags:
+		(comments)
+		DOCTYPE
+		abbr
+		acronym
+		address
+		area
+		article
+		aside
+		audio
+		b
+		base
+		basefont
+		bdi
+		bdo
+		big
+		body
+		button
+		canvas
+		caption
+		center
+		cite
+		code
+		col
+		colgroup
+		datalist
+		dd
+		del
+		details
+		dfn
+		dialog
+		dir
+		dl
+		dt
+		em
+		fieldset
+		figcaption
+		figure
+		font
+		footer
+		form
+		h1 to h6
+		header
+		html
+		i
+		input
+		ins
+		kbd
+		keygen
+		label
+		legend
+		main
+		map
+		mark
+		menu
+		menuitem
+		meter
+		nav
+		noframes
+		noscript
+		optgroup
+		option
+		output
+		param
+		progress
+		q
+		rp
+		rt
+		ruby
+		s
+		samp
+		section
+		select
+		small
+		source
+		span
+		strike
+		strong
+		sub
+		summary
+		sub
+		summary
+		sup
+		tbody
+		textarea
+		tfoot
+		thead
+		time
+		track
+		tt
+		u
+		var
+		video
+		wbr
+
+	Skipped tags:
+		applet
+		embed
+		frame
+		frameset
+		head
+		iframe
+		link
+		meta
+		object
+		script
+		style
+		title
+
+
+	Handled tags:
+		a
+		blockquote
+		br
+		div
+		hr
+		img
+		li
+		ol
+		p
+		pre
+		table
+		td
+		th
+		tr
+*/
+package unhtml
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"io"
+	"strings"
+)
+
+// HtmlToText converts the HTML in the reader to text in the writer.
+func HtmlToText(in io.Reader, out io.Writer) error {
+
+	z := html.NewTokenizer(in)
+
+	var lastTT html.TokenType = html.CommentToken
+	var newLines int = 0
+	var url string = ""
+	var ignore bool = false
+	var reallyIgnore bool = false
+
+	for done := false; !done; {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			err := z.Err()
+			if err != io.EOF {
+				return err
+			}
+			done = true
+		case html.TextToken:
+			if !ignore && !reallyIgnore {
+				text := strings.TrimSpace(string(z.Text()))
+				fmt.Fprint(out, text)
+				if text != "" || url != "" {
+					newLines = 0
+				}
+				if url != "" {
+					fmt.Fprintf(out, " (%s) ", url)
+					url = ""
+				}
+			}
+		case html.StartTagToken:
+			tag, hasAttr := z.TagName()
+			if lastTT == html.TextToken && newLines == 0 {
+				fmt.Fprint(out, " ")
+			}
+			switch string(tag) {
+			case "head":
+				reallyIgnore = true
+			case "script", "title", "link", "meta", "applet", "embed", "frame", "frameset", "iframe", "object", "style":
+				ignore = true
+			case "div", "ul", "tr", "ol", "p", "br", "pre", "table", "blockquote":
+				if newLines < 2 {
+					fmt.Fprintln(out)
+					newLines++
+				}
+			case "li":
+				if newLines < 2 {
+					fmt.Fprintln(out)
+				}
+				fmt.Fprint(out, "* ")
+				newLines = 0
+			case "hr":
+				if newLines < 2 {
+					fmt.Fprintln(out)
+				}
+				fmt.Fprintln(out, "---")
+				newLines = 1
+			case "td", "th":
+				fmt.Fprint(out, " ")
+				newLines = 0
+			case "a":
+				if hasAttr {
+					var key, val []byte
+					more := true
+					for more == true {
+						key, val, more = z.TagAttr()
+						if string(key) == "href" {
+							url = string(val)
+						}
+					}
+				}
+				newLines = 0
+			case "img":
+				if hasAttr {
+					var key, val []byte
+					more := true
+					src := ""
+					alt := ""
+					for more == true {
+						key, val, more = z.TagAttr()
+						if string(key) == "src" {
+							src = string(val)
+						} else if string(key) == "alt" {
+							alt = string(val)
+						}
+					}
+					if alt != "" {
+						fmt.Fprintf(out, alt)
+					}
+					if src != "" {
+						fmt.Fprintf(out, " (%s) ", src)
+					}
+				}
+				newLines = 0
+			}
+		case html.EndTagToken:
+			tag, _ := z.TagName()
+			switch string(tag) {
+			case "head":
+				reallyIgnore = false
+			case "script", "title", "link", "meta", "applet", "embed", "frame", "frameset", "iframe", "object", "style":
+				ignore = false
+			case "ul", "ol", "pre", "table", "blockquote":
+				if newLines < 2 {
+					fmt.Fprintln(out, "\n")
+					newLines += 2
+				}
+			case "hr":
+				if newLines < 2 {
+					fmt.Fprintln(out)
+					newLines++
+				}
+			}
+		case html.SelfClosingTagToken:
+			tag, hasAttr := z.TagName()
+			switch string(tag) {
+			case "div", "li", "br", "p":
+				if newLines < 2 {
+					fmt.Fprintln(out)
+					newLines++
+				}
+			case "hr":
+				if newLines < 2 {
+					fmt.Fprintln(out, "\n")
+				}
+				fmt.Fprintln(out, "---\n")
+				newLines = 2
+			case "a":
+				if hasAttr {
+					var key, val []byte
+					more := true
+					for more == true {
+						key, val, more = z.TagAttr()
+						if string(key) == "href" {
+							url = string(val)
+						}
+					}
+				}
+				newLines = 0
+			case "img":
+				if hasAttr {
+					var key, val []byte
+					more := true
+					src := ""
+					alt := ""
+					for more == true {
+						key, val, more = z.TagAttr()
+						if string(key) == "src" {
+							src = string(val)
+						} else if string(key) == "alt" {
+							alt = string(val)
+						}
+					}
+					s := " "
+					if newLines > 0 {
+						s = ""
+					}
+					if alt != "" {
+						fmt.Fprintf(out, "%s%s ", s, alt)
+						s = ""
+					}
+					if src != "" {
+						fmt.Fprintf(out, "%s(%s) ", s, src)
+					}
+				}
+				newLines = 0
+			}
+		case html.DoctypeToken:
+		case html.CommentToken:
+		default:
+			return fmt.Errorf("Unhandled token type: %s", tt.String())
+		}
+		lastTT = tt
+	}
+
+	if lastTT == html.TextToken && url != "" {
+		fmt.Fprintf(out, " (%s) ", url)
+	}
+
+	return nil
+}
